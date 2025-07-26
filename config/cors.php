0bc756a2064d9405edc62d2e16ef841052057296
tienuxxm <156863650+tienuxxm@users.noreply.github.com>
2025-07-26 11:25:23 +0700

    Updated cors.php

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://bitexweb.netlify.app'], // ✅ Thay bằng domain Netlify
+    'allowed_origins' => [*], // ✅ Thay bằng domain Netlify
 
     'allowed_headers' => ['*'],
 
